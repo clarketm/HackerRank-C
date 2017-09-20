9a2af4eb3b02cf9335d34c45f8cd67e5676566f7
Travis Clarke <travis.m.clarke@gmail.com>
2017-09-19 17:10:28 -0700

    add hello_world.cpp solution

---
+++ intro/hello_world.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+// Say "Hello, World!" With C++
+// https://www.hackerrank.com/challenges/cpp-hello-world/problem
+
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
+
