536233233588d9ea7a51d16a39fbd74b8abbb9d9
Travis Clarke <travis.m.clarke@gmail.com>
2017-09-19 17:17:50 -0700

    add input_output.cpp solution

---
+++ intro/input_output.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+// Input and Output
+// https://www.hackerrank.com/challenges/cpp-input-and-output/problem
+
+int main() {
+    int a, b, c, sum;
+
+    cin >> a;
+    cin >> b;
+    cin >> c;
+
+    cout << a+b+c;
+
+    return 0;
+}
